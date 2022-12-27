e8555301bb7f26a4ada3e71bdd921985de75a5c5
erulan14 <erulan14@yandex.ru>
2022-12-27 17:36:46 +0600

    Fix response

--- glot/app.py
+++ glot/app.py
@@ -1,5 +1,5 @@
 from starlette.requests import Request
-from starlette.responses import HTMLResponse, JSONResponse
+from starlette.responses import *
 from .types import Scope, Receive, Send
 from parse import parse
 
@@ -18,36 +18,56 @@ class App:
 
     @staticmethod
     def __default_response():
-        """
-        Return Not Found
-        """
-        response = HTMLResponse("<h1>Not Found</h1>")
-        response.status_code = 404
-        return response
+        """Return Not Found"""
+        return HTMLResponse("<h1>Not Found</h1>", status_code=404)
 
-    def __find_handler(self, request_path):
+    @staticmethod
+    def __method_not_allowed():
+        """Return Method not Allowed"""
+        return HTMLResponse("<h1>Method Not Allowed</h1>", status_code=405)
+
+    def __find_handler(self, request_path, request):
         """
 
         :param request_path:
         :return hanlder and parse_result:
         """
-        for path, handler in self.__routes.items():
+        for path, obj in self.__routes.items():
             parse_result = parse(path, request_path)
+            handler = obj.get("handler")
             if parse_result is not None:
-                return handler, parse_result.named
-        return None, None
+                if request.method in obj.get("methods"):
+                    return handler, request.method, parse_result.named,
+                else:
+                    return handler, None, parse_result.named
+        return None, None, None
 
     def __handle_request(self, request):
-        handler, kwargs = self.__find_handler(request_path=request.scope.get("path"))
+        handler, method, kwargs = self.__find_handler(
+            request_path=request.scope.get("path"),
+            request=request
+        )
         if handler is not None:
-            response = handler(request, **kwargs)
+            if method is not None:
+                response = handler(request, **kwargs)
+
+                if isinstance(response, str):
+                    response = HTMLResponse(response)
+
+                if isinstance(response, (list, dict)):
+                    response = JSONResponse(response)
+            else:
+                response = self.__method_not_allowed()
         else:
             response = self.__default_response()
         return response
 
-    def route(self, path):
+    def route(self, path, methods=None):
+        if methods is None:
+            methods = ["GET"]
+
         def wrapper(handler):
-            self.__routes[path] = handler
+            self.__routes[path] = {"methods": methods, "handler": handler}
             return handler
         return wrapper
 
--- main.py
+++ main.py
@@ -4,20 +4,35 @@ from glot.response import HTMLResponse
 from glot.templating import Jinja2Templates
 from datetime import datetime
 
+from pydantic import BaseModel
+
 app = Glot()
 templates = Jinja2Templates(directory="templates")
+
+
+class User(BaseModel):
+    username: str
+    password: str
+
+
+users = [User(username=f"test{i}", password="").dict() for i in range(10)]
 
 
 @app.route("/")
 def start(request):
     params = request.query_params
-    print(params)
     return templates.TemplateResponse("index.html", {"request": request, "datetime": datetime.now()})
 
 
 @app.route("/test/{name}")
 def test(request, name):
     return HTMLResponse(f"<h1>{name}</h1>")
+
+
+@app.route("/api/v1/", methods=["POST"])
+def api(request):
+    test = request.query_params.get("test")
+    return users
 
 
 if __name__ == "__main__":
